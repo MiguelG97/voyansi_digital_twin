0fd272d08bba61fdeae5791865b546fcbf02a501
miguel <miguel.gutierrezt@pucp.pe>
2024-09-18 23:43:00 -0500

    Initial Commit

---
+++ voyansi/voyansiApp.swift
@@ -0,0 +1,17 @@
+//
+//  voyansiApp.swift
+//  voyansi
+//
+//  Created by Miguel Gutiérrez on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct voyansiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
