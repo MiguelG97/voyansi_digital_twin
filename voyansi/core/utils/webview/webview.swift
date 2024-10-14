9d717765b6952b2ee6d20a9a570ee0cc7f9d123c
miguel <miguel.gutierrezt@pucp.pe>
2024-10-13 22:13:21 -0500

    WebView

---
+++ voyansi/core/utils/webview/webview.swift
@@ -0,0 +1,15 @@
+//
+//  webview_controller.swift
+//  voyansi
+//
+//  Created by Miguel Gutiérrez on 13/10/24.
+//
+import WebKit
+import SwiftUI
+
+/// UIViewController:
+/// It lets you create a SwiftUI view based on a UIKIT view
+struct WebViewController: UIViewController{
+    
+    
+}
